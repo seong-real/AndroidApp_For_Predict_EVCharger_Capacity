ae59676b56c3aa1fa37455032c6778fbf9fdd423
seong_real <wjstjdfuf98@naver.com>
2023-08-25 12:38:07 +0900

    APIService하는 부분 수정완료

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,21 +1,29 @@
+import 'package:tmap_raster_flutter_sample/models/station_info.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
 
-/*class ApiService {
-  static const String baseUrl =
-      "https://webtoon-crawler.nomadcoders.workers.dev";
-  static const String today = "today";
+class ApiService {
+  static const double latitude = 37.4920;
+  static const double longitude = 126.9260;
+  static var time = DateTime.now();
+  String formatDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(time);
+  static const String baseUrl = "http://hwangpeng.kro.kr/";
+  static const String station =
+      "station?latitude=$latitude&longitude=$longitude";
 
   static Future<List<StationInfo>> getStationInfo() async {
-    List<StationInfo> webtoonInstances = [];
-    final url = Uri.parse('$baseUrl/$today');
+    List<StationInfo> stationinfo = [];
+    final url = Uri.parse('$baseUrl/$station');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final webtoons = jsonDecode(response.body);
-      for (var webtoon in webtoons) {
-        final instance = StationInfo.fromJson(webtoon);
-        webtoonInstances.add(instance);
+      final infos = jsonDecode(response.body);
+      for (var infos in infos) {
+        final instance = StationInfo.fromJson(infos);
+        stationinfo.add(instance);
       }
-      return webtoonInstances;
+      return stationinfo;
     }
     throw Error();
   }
-}*/
+}
